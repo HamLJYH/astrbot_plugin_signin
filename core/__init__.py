30ce867aa6c6117a8f3a2b5ad37cecf5e6ce82ff
HamLJYH <3877792948@qq.com>
2026-08-22 18:45:02 +0800

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,39 @@
+"""
+AstrBot 签到插件 - 核心模块
+
+版本: 2.0.0
+"""
+
+from .constants import *
+from .models import *
+from .config import PluginConfig
+
+__all__ = [
+    "TZ_BEIJING",
+    "MAX_NICKNAME_LENGTH",
+    "MAX_HISTORY_LENGTH",
+    "MAX_TRANSFER_HISTORY",
+    "MILESTONES",
+    "SHOP_ITEMS",
+    "DAILY_LIMIT_ITEMS",
+    "FORTUNES",
+    "FORTUNE_WEIGHTS",
+    "FORTUNE_EMOJIS",
+    "LUCKY_COLORS",
+    "LUCKY_DIRECTIONS",
+    "LOTTERY_PRIZES",
+    "RANK_EMOJIS",
+    "MessageEmoji",
+    "QQ_NUMBER_PATTERN",
+    "NUMBERS_PATTERN",
+    "DB_TABLES",
+    "LEGACY_DATA_FILENAME",
+    "UserData",
+    "SigninRecord",
+    "InventoryItem",
+    "TransferRecord",
+    "OperationResult",
+    "SigninResult",
+    "GlobalStats",
+    "PluginConfig",
+]
