ea03e24be2ec4193a4e1b1d18c7efb85924855b7
HamLJYH <3877792948@qq.com>
2026-08-29 09:10:48 +0800

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,5 @@
 """
-AstrBot 签到插件 - 核心模块
-
-版本: 2.0.0
+AstrBot 签到插件 - 核心模块 
 """
 
 from .constants import *
@@ -9,31 +7,11 @@ from .models import *
 from .config import PluginConfig
 
 __all__ = [
-    "TZ_BEIJING",
-    "MAX_NICKNAME_LENGTH",
-    "MAX_HISTORY_LENGTH",
-    "MAX_TRANSFER_HISTORY",
-    "MILESTONES",
-    "SHOP_ITEMS",
-    "DAILY_LIMIT_ITEMS",
-    "FORTUNES",
-    "FORTUNE_WEIGHTS",
-    "FORTUNE_EMOJIS",
-    "LUCKY_COLORS",
-    "LUCKY_DIRECTIONS",
-    "LOTTERY_PRIZES",
-    "RANK_EMOJIS",
-    "MessageEmoji",
-    "QQ_NUMBER_PATTERN",
-    "NUMBERS_PATTERN",
-    "DB_TABLES",
-    "LEGACY_DATA_FILENAME",
-    "UserData",
-    "SigninRecord",
-    "InventoryItem",
-    "TransferRecord",
-    "OperationResult",
-    "SigninResult",
-    "GlobalStats",
-    "PluginConfig",
+    "TZ_BEIJING", "MAX_NICKNAME_LENGTH", "MAX_HISTORY_LENGTH", "MAX_TRANSFER_HISTORY",
+    "MILESTONES", "SHOP_ITEMS", "DAILY_LIMIT_ITEMS", "FORTUNES", "FORTUNE_WEIGHTS",
+    "FORTUNE_EMOJIS", "LUCKY_COLORS", "LUCKY_DIRECTIONS", "LOTTERY_PRIZES",
+    "RANK_EMOJIS", "MessageEmoji", "QQ_NUMBER_PATTERN", "NUMBERS_PATTERN",
+    "DB_TABLES", "LEGACY_DATA_FILENAME",
+    "UserData", "SigninRecord", "InventoryItem", "TransferRecord",
+    "OperationResult", "SigninResult", "GlobalStats", "PluginConfig",
 ]
